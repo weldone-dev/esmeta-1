03fad96f4bd0bf21978bde08d5b2471406943ccc
Kanguk Lee <p51lee@kaist.ac.kr>
2024-11-20 00:40:32 +0900

    Add Script.isBug

--- src/main/scala/esmeta/es/Script.scala
+++ src/main/scala/esmeta/es/Script.scala
@@ -1,4 +1,23 @@
 package esmeta.es
 
 /** ECMAScript script program */
-case class Script(code: String, name: String) extends ESElem
+case class Script(code: String, name: String, isBug: Boolean = false)
+  extends ESElem
+
+/** Script with minified tag */
+case class ScriptM(
+  code: String,
+  name: String,
+  minified: Option[Boolean] = None,
+) extends ESElem
+
+object ScriptConversions {
+
+  // Implicit conversion from Script to ScriptM
+  implicit def scriptToScriptM(script: Script): ScriptM =
+    ScriptM(script.code, script.name)
+
+  // Implicit conversion from ScriptM to Script
+  implicit def scriptMToScript(scriptM: ScriptM): Script =
+    Script(scriptM.code, scriptM.name)
+}
