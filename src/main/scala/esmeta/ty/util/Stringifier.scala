cbc5789693bcbdbe7018b9e33c092d7544942f55
Minseok Choe <choems@outlook.com>
2024-12-07 02:53:35 +0900

    fix: tymodel string should be normalized, see: <SYNTAX>:ConditionalExpression[1,0].Evaluation

--- src/main/scala/esmeta/ty/util/Stringifier.scala
+++ src/main/scala/esmeta/ty/util/Stringifier.scala
@@ -97,7 +97,7 @@ object Stringifier {
   /** unknown types */
   given unknownTyRule: Rule[UnknownTy] = (app, ty) =>
     app >> "Unknown"
-    ty.msg.fold(app)(app >> "[\"" >> _ >> "\"]")
+    ty.msg.fold(app)(app >> "[\"" >> normStr(_) >> "\"]")
 
   // predefined types
   private lazy val predTys: List[(ValueTy, String)] = List(
